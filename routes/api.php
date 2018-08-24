efc0320c3a4d4dae8f03fad297e414736fa67f27
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 21:54:21 +0430

    define action

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 Route::group(['as' => 'v1.', 'prefix' => 'v1'], function () {
     Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
         Route::group(['prefix' => 'vendor', 'as' => 'vendor.'], function () {
-            Route::get('/', ['as' => 'list']);
+            Route::get('/', ['as' => 'list', 'uses' => 'VendorController@index']);
         });
     });
 });
