0f3e5d5a8730a338323918e5769ba21400686ca7
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-25 00:16:22 +0430

    pass pay test

--- app/Http/Controllers/User/WalletController.php
+++ app/Http/Controllers/User/WalletController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Services\Wallet\WalletService;
+use App\User;
+use App\Vendor;
 use App\Wallet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class WalletController extends Controller
 {
     public function index()
     {
         return Wallet::where('user_id', Auth::user()->getAuthIdentifier())->where('user_type', 1)->get();
+    }
+
+    public function pay(Request $request)
+    {
+        /** @var User $user */
+        $user = Auth::user();
+        /** @var Vendor $vendor */
+        $vendor = Vendor::where('vendor_id', $request->input('vendor_id'))->firstOrFail();
+
+        $amount = $request->input('amount');
+
+        DB::transaction(function () use ($amount, $user, $vendor) {
+            /** @var WalletService $wallet */
+            $wallet = new WalletService();
+            $wallet->debtor($user, $amount, 1, "Pay to a vendor");
+            $wallet->creditor($vendor, $amount, 2, "Pay be a customer");
+        });
     }
 }
