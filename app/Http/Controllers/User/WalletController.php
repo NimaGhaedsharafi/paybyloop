52ad58570454e4bb16898ff8d5e2557d8258365f
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 22:25:22 +0430

    pass test for now

--- app/Http/Controllers/User/WalletController.php
+++ app/Http/Controllers/User/WalletController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Wallet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class WalletController extends Controller
 {
     public function index()
     {
-        
+        return Wallet::where('user_id', Auth::user()->getAuthIdentifier())->where('user_type', 1)->get();
     }
 }
