e8761338613c4ca3264d53d04fb6c46e83fe2c46
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 13:42:32 +0430

    define users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->nullable()->unique();
             $table->string('password');
+            $table->string('cellphone')->unique();
+            $table->string('email')->index();
+            $table->integer('cellphone_verified');
+            $table->integer('email_verified');
             $table->rememberToken();
             $table->timestamps();
         });
