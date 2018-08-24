631d71f0f57755388f09439765f470582a0378f0
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 22:30:05 +0430

    make it a better test

--- tests/Feature/User/WalletTest.php
+++ tests/Feature/User/WalletTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\User;
 
+use App\Services\Wallet\WalletService;
 use App\User;
+use App\Wallet;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\Auth;
@@ -22,11 +24,18 @@ class WalletTest extends TestCase
      */
     public function get_list_wallet_transaction()
     {
+        /** @var User $user */
         $user = factory(User::class)->create();
         Auth::login($user);
-        
+
+        $wallet = new WalletService();
+        for ($i = 0; $i < 5; $i++) {
+            $wallet->creditor($user, rand(1, 10) * 1000, 1, "");
+        }
+
+
         $this->json('GET', route('v1.user.wallet.list'))
             ->assertOk()
-            ->assertJson([]);
+            ->assertJsonCount(5);
     }
 }
