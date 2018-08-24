d6345154b765ece180a093259beac7459241d2cf
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 23:37:51 +0430

    move vendor test to user namespace

--- tests/Feature/User/VendorTest.php
+++ tests/Feature/User/VendorTest.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\User;
 
 use App\Vendor;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Tests\TestCase;
+use Tests\Feature\FeatureCase;
 
-class VendorTest extends TestCase
+class VendorTest extends FeatureCase
 {
     use WithoutMiddleware;
     use DatabaseTransactions;
