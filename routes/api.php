0fb7f2c4c6fab28109b0c42340ddeb5133bc70fa
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 22:59:33 +0430

    fix route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 Route::group(['as' => 'v1.', 'prefix' => 'v1'], function () {
     Route::group(['prefix' => 'user', 'as' => 'user.', 'namespace' => 'User'], function () {
         Route::group(['prefix' => 'profile', 'as' => 'profile.'], function () {
-            Route::get('/', ['as' => 'show', 'uses' => 'ProfileController@index']);
+            Route::get('/', ['as' => 'show', 'uses' => 'ProfileController@show']);
         });
         Route::group(['prefix' => 'vendor', 'as' => 'vendor.'], function () {
             Route::get('/', ['as' => 'list', 'uses' => 'VendorController@index']);
