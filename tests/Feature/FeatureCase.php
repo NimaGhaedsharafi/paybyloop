f4ad5f0903a596cd4eff094d02eba4bd63adb11d
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 23:56:48 +0430

    add CreateVendor method to featureCase

--- tests/Feature/FeatureCase.php
+++ tests/Feature/FeatureCase.php
@@ -10,6 +10,7 @@ namespace Tests\Feature;
 
 
 use App\User;
+use App\Vendor;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
@@ -22,6 +23,9 @@ class FeatureCase extends TestCase
     /** @var User */
     protected $user;
 
+    /** @var Vendor */
+    protected $vendor;
+
     /**
      * impersonate into a new user
      * @param array $options
@@ -32,5 +36,14 @@ class FeatureCase extends TestCase
         Auth::login($this->user = factory(User::class)->create($options));
 
         return $this->user;
+    }
+
+    /**
+     * @param array $options
+     * @return Vendor
+     */
+    public function createVendor($options = [])
+    {
+        return $this->vendor = factory(Vendor::class)->create($options);
     }
 }
