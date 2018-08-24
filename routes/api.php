a307f1c3b35e4abda341dd0fa6f52105b43d9d50
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 21:54:32 +0430

    let's have a namespace there

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 
 Route::group(['as' => 'v1.', 'prefix' => 'v1'], function () {
-    Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+    Route::group(['prefix' => 'user', 'as' => 'user.', 'namespace' => 'User'], function () {
         Route::group(['prefix' => 'vendor', 'as' => 'vendor.'], function () {
             Route::get('/', ['as' => 'list', 'uses' => 'VendorController@index']);
         });
