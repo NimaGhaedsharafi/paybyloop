14dbf53d29a6b40dba824961c53f5170b9556516
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 21:52:26 +0430

    define routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['as' => 'v1.', 'prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+        Route::group(['prefix' => 'vendor', 'as' => 'vendor.'], function () {
+            Route::get('/', ['as' => 'list']);
+        });
+    });
 });
