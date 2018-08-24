b4302469774b75b50804c31be2457a7a5df61506
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 21:45:56 +0430

    define get vendors tests

---
+++ tests/Feature/VendorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Vendor;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class VendorTest extends TestCase
+{
+    use WithoutMiddleware;
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    public function get_list_of_vendors_works()
+    {
+        $this->json('GET', route('v1.user.vendor.list'))
+            ->assertOk()
+            ->assertJson(Vendor::all());
+    }
+}
