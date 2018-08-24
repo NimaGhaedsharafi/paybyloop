f9bea3ff92ff06be38b8b64c5bdca86c2a1a8db6
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 16:40:13 +0430

    vendor implements payable interface

--- app/Vendor.php
+++ app/Vendor.php
@@ -2,9 +2,24 @@
 
 namespace App;
 
+use App\Services\Wallet\Payable;
 use Illuminate\Database\Eloquent\Model;
 
-class Vendor extends Model
+class Vendor extends Model implements Payable
 {
-    //
+    /**
+     * @return integer
+     */
+    public function getType()
+    {
+        return 2;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
