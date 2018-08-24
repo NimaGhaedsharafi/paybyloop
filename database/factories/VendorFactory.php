70369789cd29cd52da8581d8bdb46c57ff61fcdf
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 21:39:07 +0430

    define vendor 	factory

---
+++ database/factories/VendorFactory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 8/24/18
+ * Time: 9:33 PM
+ */
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Vendor::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'vendor_id' => str_random(6),
+        'address' => $faker->address,
+        'coordinate' => '35.7698678,51.3739048',
+        'phone' => $faker->phoneNumber,
+        'description' => $faker->text,
+        'photo' => $faker->imageUrl(),
+    ];
+});
