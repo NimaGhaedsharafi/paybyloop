ff6d134e138656778b1c272e478891ca891479ce
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-24 21:16:01 +0430

    add more fields to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,8 +16,12 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'username' => $faker->userName,
+        'cellphone' => $faker->numerify('+989##########'),
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'cellphone_verified' => 1,
+        'email_verified' => 1,
         'remember_token' => str_random(10),
     ];
 });
