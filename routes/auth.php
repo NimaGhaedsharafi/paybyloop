0c659c9f6bb96372707af84cf3cf66675a4ae6b6
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-08-30 22:10:50 +0430

    Remove useless namespace

--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Routing\Route;
-
 Route::post('login', 'AuthController@login');
 Route::post('refresh', 'AuthController@refresh');
 Route::post('logout', 'AuthController@logout');
