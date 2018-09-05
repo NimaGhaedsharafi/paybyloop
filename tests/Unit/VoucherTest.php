7851cf2e49f99b414845d2b6fd0ef26b884a1d5c
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-09-05 18:53:46 +0430

    define voucher test

---
+++ tests/Unit/VoucherTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\VoucherService;
+
+class VoucherTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function create_voucher()
+    {
+        // fixture
+        $amount =  rand(1, 10) * 1000;
+        $title = 'my campaign';
+        $expiresIn = Carbon::now()->addDay();
+        $code = null;
+        $maxUseTime = rand(1, 10);
+
+        /** @var VoucherService $voucher */
+        $voucher = new VoucherService();
+
+        $voucher->create($amount, $title, $expiresIn, $code, $maxUseTime);
+
+        $this->assertDatabaseHas('vouchers', [
+            'amount' => $amount,
+            'title' => $title,
+            'expires_in' => $expiresIn,
+            'code' => $code,
+            'maxUseTime' => $maxUseTime
+        ]);
+    }
+}
