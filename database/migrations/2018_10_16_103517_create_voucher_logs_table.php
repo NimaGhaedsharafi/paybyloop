27ee6428d5d93ac03b4b4d217331afc55c655792
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-10-16 14:05:52 +0330

    define base of VoucherLog

---
+++ database/migrations/2018_10_16_103517_create_voucher_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVoucherLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voucher_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voucher_logs');
+    }
+}
