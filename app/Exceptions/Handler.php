ea71dc9e2d7d46d37e7f1dff0b1cd76285d7d13b
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-10-19 13:25:44 +0330

    add exceptions to exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenExpiredException) {
+            return response()->json(['token expired'], 401);
+        } elseif ($exception instanceof TokenInvalidException) {
+            return response()->json(['token is invalid'], 401);
+        } elseif ($exception instanceof JWTException) {
+            return response()->json(['token is wrong'], 401);
+        }
+
         return parent::render($request, $exception);
     }
 }
