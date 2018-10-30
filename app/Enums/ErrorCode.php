fe4e9a8113dbd422ccbf5920c924c3a22bd32c3b
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-10-30 20:30:00 +0330

    introduce errorCode Enum

---
+++ app/Enums/ErrorCode.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 10/30/18
+ * Time: 8:20 PM
+ */
+
+namespace App\Enums;
+
+/**
+ * Class ErrorCode
+ * @package App
+ */
+class ErrorCode
+{
+    const InvalidOTPToken = 1001;
+}
