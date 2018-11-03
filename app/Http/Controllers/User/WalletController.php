91e4b092e960047b04227b351c56f0b5c21334ce
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-11-03 22:49:31 +0330

    fix wallet history issue

--- app/Http/Controllers/User/WalletController.php
+++ app/Http/Controllers/User/WalletController.php
@@ -17,7 +17,7 @@ class WalletController extends Controller
 {
     public function index()
     {
-        return Wallet::where('user_id', Auth::user()->getAuthIdentifier())->where('user_type', 1)->get();
+        return Wallet::where('user_id', Auth::user()->id)->where('user_type', 1)->get();
     }
 
     public function pay(Request $request)
