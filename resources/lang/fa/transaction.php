a3cfb8b0bf60885da09e992a30a596b049a67e70
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-12-22 19:22:46 +0330

    add missing translation file

---
+++ resources/lang/fa/transaction.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 12/22/18
+ * Time: 7:11 PM
+ */
+
+return [
+    'payment' => 'پرداخت به :name',
+    'ipg' => 'شارژ کیف پول',
+    'giftcard' => 'شارژ با کارت هدیه'
+];
