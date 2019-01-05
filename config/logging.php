a8d5d05580eb4361a5f9159f8ff6347f61d6ad5b
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2019-01-05 23:19:21 +0330

    change log driver to daily

--- config/logging.php
+++ config/logging.php
@@ -35,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'rollbar'],
+            'channels' => ['daily', 'rollbar'],
         ],
 
         'single' => [
